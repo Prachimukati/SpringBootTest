150239c3c7b07626dd41df8d7c469ed53f009227
Prachi Mukati <prachimukati50@gmail.com>
2025-01-02 13:33:19 +0530

    02/01/2025

---
+++ spring-boot-testt/src/main/java/com/rays/SpringBootTesttApplication.java
@@ -0,0 +1,15 @@
+package com.rays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTesttApplication {
+	
+	public static void main(String[] args) {
+		
+		SpringApplication.run(SpringBootTesttApplication.class, args);
+		
+	}
+
+}
